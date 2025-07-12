620c44e6b652f8415e37584db261db56f867ca94
DESKTOP-KO9ALAI\SamuchPC <samuch.dev@gmail.com>
2025-07-12 14:36:50 +0700

    first commit

---
+++ src/test/java/com/javaverse/course/spring_ninja/SpringNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaverse.course.spring_ninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
