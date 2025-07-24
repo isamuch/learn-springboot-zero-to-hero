ceba1393f82993f52d63fe7adede73f0ea37caa0
DESKTOP-KO9ALAI\SamuchPC <samuch.dev@gmail.com>
2025-07-24 22:26:05 +0700

    [Step 13] Tip exclude listeners

--- src/main/java/com/javaverse/course/springninja/entity/DryEntityExample.java
+++ src/main/java/com/javaverse/course/springninja/entity/DryEntityExample.java
@@ -1,15 +1,13 @@
 package com.javaverse.course.springninja.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
+// @ExcludeDefaultListeners กรณ๊ที่ต้องการ fields แต่ไม่อยากได้ Listeners
 public class DryEntityExample extends CommonEntity {
 
     @Id
